e2a5fbe0a30c09688385eaad654daa36de950edc
guivmc <guivmc@hotmail.com>
2019-04-12 11:00:57 -0300

    Add files via upload

---
+++ Test.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <title>Post</title>
+    </head>
+    <body>
+        <pre>
+            <?php
+            //require_once './index.php';
+            
+            class Prod {
+
+                private $name;
+                private $description;
+                private $value;
+                private $amount;
+
+                public function __construct($name, $description, $value, $amount) {
+                    $this->name = $name;
+                    $this->description = $description;
+                    $this->value = $value;
+                    $this->amount = $amount;
+                }
+
+                function getName() {
+                    return $this->name;
+                }
+
+                function setName($name) {
+                    $this->name = $name;
+                }
+
+                function getDesc() {
+                    return $this->description;
+                }
+
+                function setDesc($desc) {
+                    $this->description = $desc;
+                }
+
+                function getValue() {
+                    return $this->value;
+                }
+
+                function setValue($value) {
+                    $this->value = $value;
+                }
+
+                function getAmt() {
+                    return $this->amount;
+                }
+
+                function setAmt($amount) {
+                    $this->amount = $amount;
+                }
+
+            }
+            
+            $obj = new Prod($_POST["name"], $_POST["description"], $_POST["value"], $_POST["amount"]);
+            
+            echo "Nome: " . $obj->getName() . "</br>";
+            echo "Descr.: " . $obj->getDesc() . "</br>";
+            echo "Preço: " . $obj->getValue() . "</br>";
+            echo "Quantia: " . $obj->getAmt() . "</br>";
+           
+            ?>
+        </pre>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PHP</title>
+    </head>
+    <body>
+        
+        <form method="post" action="Test.php">
+            <input placeholder="Nome" name="name"/>
+            <input placeholder="Descrição" name="description"/>
+            <input type="number" placeholder="Preço" name="value"/>
+            <input type="number" placeholder="Quantidade" name="amount"/>
+            <input type="submit" value="Salvar" />
+        </form>
+        
+    </body>
+</html>
